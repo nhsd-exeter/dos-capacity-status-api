e2a7874a0616edc4e4392599b75596f98377d1d7
Matthew Begley <60427904+mabe13@users.noreply.github.com>
2020-02-04 12:20:10 +0000

    Add DoS auth models & initial auth app setup (#4)
    * Add DoS auth models & initial auth setup
    
    * removed not needed fields & models

---
+++ application/api/capacityauth/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+from api.dos.models import Users, Permissions, Userpermissions
+from api.dos.models import Services, Userservices
---
+++ application/api/dos/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+
+class Services(models.Model):
+
+    uid = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    statusid = models.IntegerField(blank=True, null=True)
+    publicname = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = "services"
+
+
+class Capacitystatuses(models.Model):
+    capacitystatusid = models.AutoField(primary_key=True)
+    color = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = "capacitystatuses"
+
+
+class Users(models.Model):
+    username = models.CharField(unique=True, max_length=255)
+    email = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=255, blank=True, null=True)
+    homeorganisation = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = "users"
+
+
+class ServiceCapacities(models.Model):
+    notes = models.TextField(blank=True, null=True)
+    modifiedbyid = models.IntegerField(blank=True, null=True)
+    modifiedby = models.TextField(blank=True, null=True)
+    modifieddate = models.DateTimeField(blank=True, null=True)
+    service = models.OneToOneField(
+        Services, models.DO_NOTHING, db_column="serviceid", blank=True, null=True
+    )
+    capacitystatus = models.ForeignKey(
+        Capacitystatuses,
+        models.DO_NOTHING,
+        db_column="capacitystatusid",
+        blank=True,
+        null=True,
+    )
+    resetdatetime = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = "servicecapacities"
