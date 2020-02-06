25314d0752e8265d358b4d04b4773a5a08c0fc6a
Matthew <matthewbegley@nhs.net>
2020-02-06 16:24:16 +0000

    fix sql dos test script

--- data/sql/04-5-data-test-user-tables.sql
+++ data/sql/04-5-data-test-user-tables.sql
@@ -13,9 +13,9 @@ select 1000000001, per.id from pathwaysdos.permissions per where per.name = 'edi
 
 -- Link user to services
 insert into pathwaysdos.userservices (userid, serviceid)
-values(1000000000, '6811') -- uid = 153455
+values(1000000000, 6811); -- uid = 153455
 insert into pathwaysdos.userservices (userid, serviceid)
-values(1000000001, '6811') -- uid = 153455
+values(1000000001, 6811); -- uid = 153455
 
 -- Make some services Ancestors of the Linked services
 UPDATE pathwaysdos.services SET parentid=6811  WHERE id=2668;  -- uid = 149198
