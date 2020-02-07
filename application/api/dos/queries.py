bcd8ac28af206ec155a3f3c0d0430fc1a280d512
Matthew <matthewbegley@nhs.net>
2020-02-07 13:56:30 +0000

    Changes after pr review

--- application/api/dos/queries.py
+++ application/api/dos/queries.py
@@ -1,19 +1,19 @@
 from django.db import connections
 
 
-can_user_edit_service_sql = """SELECT EXISTS(SELECT u FROM users u
+can_user_edit_service_sql = """SELECT EXISTS(SELECT 1 FROM users u
     JOIN userpermissions up ON up.userid = u.id
     JOIN userservices us ON us.userid = u.id
     JOIN services s ON s.id = us.serviceid
     WHERE u.id = %s AND up.permissionid = 3 AND  us.serviceid in (
-        WITH RECURSIVE user_accessible_services AS (
-            select s.id, s.uid, s.parentid from services s
+        WITH RECURSIVE service_ancestry AS (
+            select s.id, s.parentid from services s
             where s.uid = %s
             union
-            select s.id, s.uid, s.parentid from services s
-            join user_accessible_services d on d.parentid = s.id
+            select s.id, s.parentid from services s
+            join service_ancestry d on d.parentid = s.id
         )
-        select id from user_accessible_services)
+        select id from service_ancestry)
     );"""
 
 
--- application/api/dos/tests/test_queries.py
+++ application/api/dos/tests/test_queries.py
@@ -1,9 +1,9 @@
-import unittest
+from unittest import TestCase
 
 from ..queries import can_user_edit_service
 
 
-class TestQueries(unittest.TestCase):
+class TestQueries(TestCase):
     "Tests for dos query functions"
 
     def test_can_user_edit_service__service_exists(self):
@@ -12,7 +12,6 @@ class TestQueries(unittest.TestCase):
         userId = 1000000001
         user_can_edit = can_user_edit_service(userId, serviceUid)
         self.assertTrue(user_can_edit)
-        print(user_can_edit)
 
     def test_can_user_edit_service__child_decendant_service_exists(self):
         "Test sql can_user_edit_service method,returns true when the service's parent is linked to the user"
@@ -20,7 +19,6 @@ class TestQueries(unittest.TestCase):
         userId = 1000000001
         user_can_edit = can_user_edit_service(userId, serviceUid)
         self.assertTrue(user_can_edit)
-        print(user_can_edit)
 
     def test_can_user_edit_service__grandchild_decendant_service_exists(self):
         "Test sql can_user_edit_service method, returns true when the service's grand parent is linked to the user"
@@ -28,7 +26,6 @@ class TestQueries(unittest.TestCase):
         userId = 1000000001
         user_can_edit = can_user_edit_service(userId, serviceUid)
         self.assertTrue(user_can_edit)
-        print(user_can_edit)
 
     def test_can_user_edit_service__is_not_linked_to_users(self):
         "Test sql can_user_edit_service method, returns false when the service does not exist"
@@ -36,7 +33,6 @@ class TestQueries(unittest.TestCase):
         userId = 1000000001
         user_can_edit = can_user_edit_service(userId, serviceUid)
         self.assertFalse(user_can_edit)
-        print(user_can_edit)
 
     def test_can_user_edit_service__does_not_exists(self):
         "Test sql can_user_edit_service method, returns false when the service isn't linked to the user directly or by ancestry"
@@ -44,7 +40,6 @@ class TestQueries(unittest.TestCase):
         userId = 1000000001
         user_can_edit = can_user_edit_service(userId, serviceUid)
         self.assertFalse(user_can_edit)
-        print(user_can_edit)
 
     def test_can_user_edit_service__does_not_have_the_right_permissions(self):
         "Test sql can_user_edit_service method, return false as the user does not have the correct permissions"
@@ -52,4 +47,3 @@ class TestQueries(unittest.TestCase):
         userId = 1000000000
         user_can_edit = can_user_edit_service(userId, serviceUid)
         self.assertFalse(user_can_edit)
-        print(user_can_edit)
