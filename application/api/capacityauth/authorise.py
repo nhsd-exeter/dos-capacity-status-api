056800998a02c80f8fd1984640312e9f52dba417
Matthew <matthewbegley@nhs.net>
2020-02-11 12:56:06 +0000

    Implementation & testing

---
+++ application/api/capacityauth/authorise.py
@@ -0,0 +1,16 @@
+from api.dos.queries import can_user_edit_service
+from .models import User, ApiDosUserAssociations, Services
+
+
+def convert_api_user_to_dos_user_id(api_user):
+    aduas = ApiDosUserAssociations.objects.filter(apiuserid=api_user.id)
+    if len(aduas) > 0:
+        return aduas[0].dosuserid
+    return None
+
+
+def can_api_user_edit_service(api_user, service):
+    # one convert api user to a dos user id
+    dos_user_id = convert_api_user_to_dos_user_id(api_user)
+    # two check against dos and return true/false
+    return can_user_edit_service(dos_user_id, service.uid)
---
+++ application/api/capacityauth/tests/__init__.py
@@ -0,0 +1,1 @@
+from .test_authorise import *
