6697f88c9923c0c1da0dbd8aaa6f1772687d2c89
Jonathan Pearce <j.pearce1@nhs.net>
2020-02-20 14:42:18 +0000

    Added admin user and capacity user group (#14)

---
+++ application/api/capacityauth/migrations/0002_create_admin_user.py
@@ -0,0 +1,29 @@
+import os
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("capacityauth", "0001_initial"),
+    ]
+
+    def generate_superuser(apps, schema_editor):
+        from django.contrib.auth.models import User
+
+        DJANGO_SU_NAME = "admin"
+        DJANGO_SU_PASSWORD = os.environ.get("APP_ADMIN_PASSWORD")
+
+        if (DJANGO_SU_PASSWORD) == None:
+            raise Exception(
+                "Admin password in APP_ADMIN_PASSWORD environment variable has not been specified"
+            )
+
+        superuser = User.objects.create_superuser(
+            username=DJANGO_SU_NAME, password=DJANGO_SU_PASSWORD,
+        )
+
+        superuser.save()
+
+    operations = [
+        migrations.RunPython(generate_superuser),
+    ]
