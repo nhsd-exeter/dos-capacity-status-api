9519f4a625fb96d69be8519de6713f91bae56872
Matthew <matthewbegley@nhs.net>
2020-02-24 15:36:46 +0000

    query and test changes

--- application/api/dos/queries.py
+++ application/api/dos/queries.py
@@ -1,18 +1,19 @@
 from django.db import connections
 from .models import Users
 
-
+# presumes active services only (statusid 1) for a user with 'Active' status
 can_user_edit_service_sql = """SELECT EXISTS(SELECT 1 FROM users u
     JOIN userpermissions up ON up.userid = u.id
     JOIN userservices us ON us.userid = u.id
     JOIN services s ON s.id = us.serviceid
-    WHERE u.id = %s AND up.permissionid = 3 AND  us.serviceid in (
+    WHERE u.id = %s AND u.status = 'ACTIVE' AND up.permissionid = 3 AND  us.serviceid in (
         WITH RECURSIVE service_ancestry AS (
             select s.id, s.parentid from services s
-            where s.uid = %s
+            where s.uid = %s AND s.statusid = 1
             union
             select s.id, s.parentid from services s
             join service_ancestry d on d.parentid = s.id
+            where s.statusid = 1
         )
         select id from service_ancestry)
     );"""
@@ -25,6 +26,7 @@ def can_user_edit_service(dos_user_id, service_uid):
         row = cursor.fetchone()
 
     return row[0]
+
 
 def get_dos_user_for_username(dos_username):
     return Users.objects.db_manager("dos").get(username=dos_username)
