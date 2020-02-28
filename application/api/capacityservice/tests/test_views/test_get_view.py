4ef6fa9b293a68d6453864f4cdeccb28bde145c1
Jonathan Pearce <j.pearce1@nhs.net>
2020-02-28 15:32:07 +0000

    Added first tests of endpoint

--- application/api/capacityservice/tests/test_views/test_get_view.py
+++ application/api/capacityservice/tests/test_views/test_get_view.py
@@ -1,7 +1,40 @@
 import unittest
+import json
+from django.test import Client
+
 
 class TestGetView(unittest.TestCase):
     "Tests for the GET view"
 
-    def test_default(self):
-        self.assertEqual(1, 1, "Test message")
+    def test_unauthorised_user_no_creds(self):
+        client = Client()
+
+        response = client.get("/api/v0.0.1/capacity/services/149198/capacitystatus/")
+
+        self.assertEqual(
+            response.status_code, 403, "Response status code is not as expected."
+        )
+        self.assertEqual(
+            response.content,
+            b'{"detail":"Authentication credentials were not provided."}',
+            "Response status code is not as expected.",
+        )
+
+    def test_unauthorised_user_invalid_creds(self):
+        client = Client()
+        auth_headers = {
+            "HTTP_AUTHORIZATION": "Api-Key xsEe8gfN.udOR2hw5P17gAXGAsMF9Q8HS45tN6naa",
+        }
+
+        response = client.get(
+            "/api/v0.0.1/capacity/services/149198/capacitystatus/",
+            HTTP_HOST="127.0.0.1",
+            **auth_headers
+        )
+        json_response = json.loads(str(response.content, encoding="utf8"))
+
+        self.assertEqual(
+            response.status_code, 200, "Response status code is not as expected."
+        )
+        self.assertEqual(json_response["serviceUid"], 149198)
+
