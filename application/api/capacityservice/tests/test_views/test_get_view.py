41c87bf149b282d683a4a2dcf51b532c798861f6
Jonathan Pearce <j.pearce1@nhs.net>
2020-03-04 09:19:19 +0000

    module tests

--- application/api/capacityservice/tests/test_views/test_get_view.py
+++ application/api/capacityservice/tests/test_views/test_get_view.py
@@ -2,14 +2,58 @@ import unittest
 import json
 from django.test import Client
 
+from .test_utils import TestUtils
+
 
 class TestGetView(unittest.TestCase):
     "Tests for the GET view"
 
-    def test_unauthorised_user_no_creds(self):
-        client = Client()
+    client = Client()
 
-        response = client.get("/api/v0.0.1/capacity/services/149198/capacitystatus/")
+    def test_unauthorised_user_no_creds(self):
+        response = self.client.get(TestUtils.api_url)
+
+        self.assertEqual(
+            response.status_code, 403, "Response status code is not as expected."
+        )
+        self.assertEqual(
+            response.content,
+            b'{"detail":"Authentication credentials were not provided."}',
+            "Response message is not as expected.",
+        )
+
+    def test_no_service_found(self):
+        response = self.client.get(
+            TestUtils.api_no_service_url,
+            HTTP_HOST="127.0.0.1",
+            **TestUtils.auth_headers,
+        )
+
+        self.assertEqual(
+            response.status_code, 404, "Response status code is not as expected."
+        )
+        self.assertEqual(
+            response.content, b'"Given service does not exist"',
+        )
+
+    def test_authorised_user(self):
+        response = self.client.get(
+            TestUtils.api_url, HTTP_HOST="127.0.0.1", **TestUtils.auth_headers
+        )
+        json_response = json.loads(str(response.content, encoding="utf8"))
+
+        self.assertEqual(
+            response.status_code, 200, "Response status code is not as expected."
+        )
+
+        self.assertEqual(json_response["serviceUid"], 149198)
+        self.assertEqual(
+            json_response["serviceName"],
+            "Dentist - Castle View Dental Practice, Dudley",
+        )
+
+    def test_put_unauthorised_user_no_creds(self):
+        response = self.client.put(TestUtils.api_url)
 
         self.assertEqual(
             response.status_code, 403, "Response status code is not as expected."
@@ -20,21 +64,141 @@ class TestGetView(unittest.TestCase):
             "Response status code is not as expected.",
         )
 
-    def test_unauthorised_user_invalid_creds(self):
-        client = Client()
-        auth_headers = {
-            "HTTP_AUTHORIZATION": "xxxxxx",
-        }
-
-        response = client.get(
-            "/api/v0.0.1/capacity/services/149198/capacitystatus/",
+    def test_put_no_service_found(self):
+        response = self.client.put(
+            TestUtils.api_no_service_url,
             HTTP_HOST="127.0.0.1",
-            **auth_headers
+            **TestUtils.auth_headers,
         )
+
+        self.assertEqual(
+            response.status_code, 404, "Response status code is not as expected."
+        )
+        self.assertEqual(
+            response.content, b'"Given service does not exist"',
+        )
+
+    """def test_invalid_capacity_status_given_number(self):
+        client = Client()
+
+        data = '{"capacityStatus":30}'
+
+        response = client.put(
+            TestUtils.api_url,
+            content_type="application/json",
+            data=data,
+            HTTP_HOST="127.0.0.1",
+            **TestUtils.auth_headers,
+        )
+
+        self.assertEqual(
+            response.status_code, 400, "Response status code is not as expected."
+        )
+
+        self.assertTrue(
+            (str(response.content).find("VAL-0002") > 0),
+            "Response message is not as expected.",
+        )"""
+
+    def test_invalid_capacity_status_given_text(self):
+        data = '{"capacityStatus":"PINK"}'
+
+        response = TestUtils.call_put_with_payload(data)
+
+        self.assertEqual(
+            response.status_code, 400, "Response status code is not as expected."
+        )
+
+        self.assertTrue(
+            (str(response.content).find("VAL-0002") > 0),
+            "Response message is not as expected.",
+        )
+
+    def test_invalid_capacity_status_given_blank(self):
+        data = '{"capacityStatus":""}'
+
+        response = self.client.put(
+            TestUtils.api_url,
+            content_type="application/json",
+            data=data,
+            HTTP_HOST="127.0.0.1",
+            **TestUtils.auth_headers,
+        )
+
+        self.assertEqual(
+            response.status_code, 400, "Response status code is not as expected."
+        )
+
+        self.assertTrue(
+            (str(response.content).find("VAL-0002") > 0),
+            "Response message is not as expected.",
+        )
+
+    def test_invalid_reset_status_in_given_too_low(self):
+        data = '{"capacityStatus":"red",\
+            "resetStatusIn": -1}'
+
+        response = self.client.put(
+            TestUtils.api_url,
+            content_type="application/json",
+            data=data,
+            HTTP_HOST="127.0.0.1",
+            **TestUtils.auth_headers,
+        )
+
+        self.assertEqual(
+            response.status_code, 400, "Response status code is not as expected."
+        )
+
+        self.assertTrue(
+            (str(response.content).find("VAL-0004") > 0),
+            "Response message is not as expected.",
+        )
+
+    def test_invalid_reset_status_in_given_too_high(self):
+        data = '{"capacityStatus":"red",\
+            "resetStatusIn": 2345}'
+
+        response = self.client.put(
+            TestUtils.api_url,
+            content_type="application/json",
+            data=data,
+            HTTP_HOST="127.0.0.1",
+            **TestUtils.auth_headers,
+        )
+
         json_response = json.loads(str(response.content, encoding="utf8"))
 
         self.assertEqual(
-            response.status_code, 200, "Response status code is not as expected."
+            response.status_code, 400, "Response status code is not as expected."
         )
-        self.assertEqual(json_response["serviceUid"], 149198)
+
+        self.assertTrue(
+            (str(response.content).find("VAL-0004") > 0),
+            "Response message is not as expected.",
+        )
+
+    """def test_invalid_capacity_status_given_none(self):
+        client = Client()
+
+        data = '{"capacityStatus":}'
+
+        response = client.put(
+            TestUtils.api_url,
+            content_type="application/json",
+            data=data,
+            HTTP_HOST="127.0.0.1",
+            **TestUtils.auth_headers,
+        )
+
+        json_response = json.loads(str(response.content, encoding="utf8"))
+
+        self.assertEqual(
+            response.status_code, 400, "Response status code is not as expected."
+        )
+
+        self.assertTrue(
+            (str(response.content).find("VAL-0002") > 0),
+            "Response message is not as expected.",
+        )"""
 
