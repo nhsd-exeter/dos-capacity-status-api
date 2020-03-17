64751445b5756f9502c6be4240a6e00b0ec54af1
Matthew <matthewbegley@nhs.net>
2020-03-17 02:49:28 +0000

    Switch to token based auth

--- application/api/capacityauth/admin.py
+++ application/api/capacityauth/admin.py
@@ -1,38 +1,75 @@
 # organizations/admin.py
 import typing
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.db import models
 from django.http.request import HttpRequest
 from rest_framework_api_key.admin import APIKeyModelAdmin
-from .models import DosUserAPIKey
+# from .models import DosUserAPIKey, CapacityUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, User
+from .models import CapacityAuthDosUser
+
 
 admin.site.site_header = "Capacity Status API Administration"
 
 
-@admin.register(DosUserAPIKey)
-class DosUserAPIKeyModelAdmin(APIKeyModelAdmin):
+# @admin.register(DosUserAPIKey)
+# class DosUserAPIKeyModelAdmin(APIKeyModelAdmin):
 
-    # form = DosUserAPIKeyAdminForm
-    is_change_view_call = False
+#     # form = DosUserAPIKeyAdminForm
+#     is_change_view_call = False
+
+#     def get_exclude(self, request, obj=None):
+#         if self.exclude:
+#             self.exclude += ["dos_user_id", "name"]
+#         else:
+#             self.exclude = ["dos_user_id", "name"]
+
+#         return super().get_exclude(request, obj)
+
+#     def get_readonly_fields(
+#         self, request: HttpRequest, obj: models.Model = None
+#     ) -> typing.Tuple[str, ...]:
+#         fields = super().get_readonly_fields(request, obj)
+#         if self.is_change_view_call:
+#             fields += ("dos_username",)
+#         return fields
+
+#     def change_view(self, request, object_id, form_url="", extra_context=None):
+#         self.is_change_view_call = True
+#         return_value = super().change_view(request, object_id, form_url, extra_context)
+#         self.is_change_view_call = False
+#         return return_value
+
+# class CapacityUserAdmin(UserAdmin):
+#     model = CapacityUser
+
+#     def get_fieldsets(self, request, obj=None):
+#         fieldset = super().get_fieldsets(request, obj)
+#         if "dos_username" not in  fieldset[0][1]["fields"]:
+#             fieldset[0][1]["fields"] += ("dos_username",)
+#         return fieldset
+
+# admin.site.register(CapacityUser, CapacityUserAdmin)
+
+class DosUserInline(admin.StackedInline):
+    model = CapacityAuthDosUser
+    can_delete = True
+    verbose_name_plural = "DoS User"
 
     def get_exclude(self, request, obj=None):
-        if self.exclude:
-            self.exclude += ["dos_user_id", "name"]
+        exclude = super().get_exclude(request, obj)
+        if exclude is None:
+            exclude = ["dos_user_id",]
         else:
-            self.exclude = ["dos_user_id", "name"]
+            exclude += ["dos_user_id",]
+        return exclude
 
-        return super().get_exclude(request, obj)
+class UserAdmin(BaseUserAdmin):
+    inlines = (DosUserInline,)
 
-    def get_readonly_fields(
-        self, request: HttpRequest, obj: models.Model = None
-    ) -> typing.Tuple[str, ...]:
-        fields = super().get_readonly_fields(request, obj)
-        if self.is_change_view_call:
-            fields += ("dos_username",)
-        return fields
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
-    def change_view(self, request, object_id, form_url="", extra_context=None):
-        self.is_change_view_call = True
-        return_value = super().change_view(request, object_id, form_url, extra_context)
-        self.is_change_view_call = False
-        return return_value
+
+
--- application/api/capacityauth/authorise.py
+++ application/api/capacityauth/authorise.py
@@ -1,13 +1,17 @@
 from django.core.exceptions import ObjectDoesNotExist
 from api.dos.queries import can_user_edit_service, get_dos_user_for_user_id
+from .models import CapacityAuthDosUser
 
+def get_dos_user(capacity_user):
 
-def can_dos_user_api_key_edit_service(dos_user_api_key, service_uid):
-    return can_user_edit_service(dos_user_api_key.dos_user_id, service_uid)
-
-
-def get_user_for_key(dos_user_api_key):
     try:
-        return get_dos_user_for_user_id(dos_user_api_key.dos_user_id)
+        dos_user = CapacityAuthDosUser.objects.db_manager("default").get(user_id=capacity_user.id)
+        return get_dos_user_for_user_id(dos_user.dos_user_id)
     except ObjectDoesNotExist:
         return None
+
+def can_capacity_user_edit_service(capacity_user, service_uid):
+    dos_user = get_dos_user(capacity_user)
+    if dos_user is None:
+        return False
+    return can_user_edit_service(dos_user.id, service_uid)
