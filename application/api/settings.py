488d15bde3aa3884f5f5d42887f8a1e57a0bfdec
Jonathan Pearce <j.pearce1@nhs.net>
2020-03-20 11:41:11 +0000

    Removed cert files

--- application/api/settings.py
+++ application/api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "=tapo65h_g^cf4sxjawp-tl&z@1@5*&)p5gn2kax!^udtvs27c"
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    ".eu-west-2.elb.amazonaws.com",
+    ".amazonaws.com",
     "localhost",
 ]
 
