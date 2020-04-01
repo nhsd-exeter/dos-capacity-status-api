8db97c1db6e0b90f0c8aaf70b41783b0dbcc38c9
Matthew <matthewbegley@nhs.net>
2020-04-01 11:51:31 +0100

    Code fixes for failing test Inparticular this makes sure token authentication is required, by using the IsAuthenticated permission class.

--- application/api/settings.py
+++ application/api/settings.py
@@ -173,6 +173,14 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
