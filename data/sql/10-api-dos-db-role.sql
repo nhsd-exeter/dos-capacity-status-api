dadf133d4cb819f619950750d7ad0ef78c2e6eba
Jonathan Pearce <j.pearce1@nhs.net>
2020-04-24 09:52:41 +0100

    Added initial creation script for DoS database user

---
+++ data/sql/10-api-dos-db-role.sql
@@ -0,0 +1,26 @@
+--
+-- This script will create a role that the Capacity Status API will use to connect to
+-- the DoS database. The role will consist of the minmimum amount of priviledges required
+-- in order for the API to retrieve and update the capacity status information of a
+-- service.
+--
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Roles
+--
+
+CREATE ROLE uec_dos_api_cs_role;
+ALTER ROLE uec_dos_api_cs WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'uec_dos_api_cs_role';
+COMMENT ON ROLE uec_dos_api_cs IS 'Accessor role for the DoS Capacity Status API.';
+
+-- User Configurations
+--
+
+ALTER ROLE CREATE ROLE uec_dos_api_cs_role; SET search_path TO 'pathwaysdos';
+
+
