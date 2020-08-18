b20d5f00913dc4ac9a7f5fcb7a6265857435843c
Jack Plowman <jack.plowman1@nhs.net>
2020-08-18 15:06:42 +0100

    Add slack notification on failure

--- application/api/dos_interface/tests/test_reporting.py
+++ application/api/dos_interface/tests/test_reporting.py
@@ -1,39 +1,61 @@
 from unittest import TestCase, mock
 from datetime import datetime
 from ..reporting import _get_request_execution_time, log_reporting_info, usage_reporting_logger
-from ..reporting import api_logger  # , _retrieve_service_data
+from ..reporting import api_logger, _retrieve_service_data
 from time import gmtime as real_gmtime, strftime
 
 
 class TestDosInterfaceReporting(TestCase):
     "Test for reporting to dos"
 
-    # def test_retrieve_service_data(self):
-    #     service_uid = "149198"
-    #     service_data = _retrieve_service_data(service_uid)
-    #     expected_keys = [
-    #         "CLIENT_NAME",
-    #         "ORG_NAME",
-    #         "ORG_TYPE",
-    #         "PARENT_ORG",
-    #         "DOS_REGION",
-    #         "CAPACITY_STATUS",
-    #         "NOTES",
-    #         "RESET_DATE_TIME",
-    #         "CHANGED_TIME",
-    #     ]
-    #     keys = list(service_data.keys())
-    #     assert keys == expected_keys
-    #     for key in keys:
-    #         assert type(service_data[key]) is str and service_data[key] != ""
-    #         if key != "CLIENT_NAME" and key != "CAPACITY_STATUS":
-    #             lower = key.lower()
-    #             no_underscore = lower.replace("_", "")
-    #             msg = "key " + key + " as lower with or without underscores is NOT present"
-    #             assert lower in service_data[key] or no_underscore in service_data[key], msg
+    reset_date_time_dummy = datetime.now()  # change to be 4 hours in the future
+    modified_date_time_dummy = datetime.now()
 
-    #     assert service_data["CLIENT_NAME"] in ["EditUser", "TestUser"]
-    #     assert "capacity" in service_data["CAPACITY_STATUS"]
+    def _mock_service_info_return_value(self):
+        service_info = {
+            "name": "dummy-service",
+            "typeid": 100,
+            "modifiedby": "dummy-client",
+            "color": "DUMMY-COLOR",
+            "notes": "dummy notes about capacity status",
+            "resetdatetime": self.reset_date_time_dummy,
+            "modifieddate": self.modified_date_time_dummy,
+        }
+        parent_service_info = {"uid": "dummy-parent-uid"}
+        dos_region_info = {"name": "dummy-dos-region-name"}
+
+        return (service_info, parent_service_info, dos_region_info)
+
+    @mock.patch("api.dos_interface.reporting.get_service_info")
+    def test_retrieve_service_data(self, mock_get_service_info):
+        mock_get_service_info.return_value = self._mock_service_info_return_value()
+        service_uid = "149198"
+        service_data = _retrieve_service_data(service_uid)
+        expected_keys = [
+            "CLIENT_NAME",
+            "ORG_NAME",
+            "ORG_TYPE",
+            "PARENT_ORG",
+            "DOS_REGION",
+            "CAPACITY_STATUS",
+            "NOTES",
+            "RESET_DATE_TIME",
+            "CHANGED_TIME",
+        ]
+        keys = list(service_data.keys())
+        assert keys == expected_keys
+        for key in keys:
+            assert (type(service_data[key]) is str and service_data[key] != "") or type(service_data[key]) is None, (
+                "key value not string or none key: " + str(key) + " value: " + str(service_data[key])
+            )
+            if key != "CLIENT_NAME" and key != "CAPACITY_STATUS":
+                lower = key.lower()
+                no_underscore = lower.replace("_", "")
+                msg = "key " + key + " as lower with or without underscores is NOT present"
+                assert lower in service_data[key] or no_underscore in service_data[key], msg
+
+        assert service_data["CLIENT_NAME"] in ["dummy-client"]
+        assert "capacity" in service_data["CAPACITY_STATUS"]
 
     def test_get_request_execution_time(self):
         class Request:
