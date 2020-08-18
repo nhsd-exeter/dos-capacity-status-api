dd1e301aa99743aaf2579b905e86cc4d4fc1861e
Matthew <matthewbegley@nhs.net>
2020-08-18 17:18:31 +0100

    Mockifying a unit test

--- application/api/dos_interface/tests/test_reporting.py
+++ application/api/dos_interface/tests/test_reporting.py
@@ -8,7 +8,27 @@ from time import gmtime as real_gmtime, strftime
 class TestDosInterfaceReporting(TestCase):
     "Test for reporting to dos"
 
-    def test_retrieve_service_data(self):
+    reset_date_time_dummy = datetime.now()  # change to be 4 hours in the future
+    modified_date_time_dummy = datetime.now()
+
+    def _mock_service_info_return_value(self):
+        service_info = {
+            "name": "dummy-service",
+            "typeid": 100,
+            "modifiedby": "dummy-client",
+            "color": "DUMMY-COLOR",
+            "notes": "dummy notes about capacity status",
+            "resetdatetime": self.reset_date_time_dummy,
+            "modifieddate": self.modified_date_time_dummy,
+        }
+        parent_service_info = {"uid": "dummy-parent-uid"}
+        dos_region_info = {"name": "dummy-dos-region-name"}
+
+        return (service_info, parent_service_info, dos_region_info)
+
+    @mock.patch("api.dos_interface.reporting.get_service_info")
+    def test_retrieve_service_data(self, mock_get_service_info):
+        mock_get_service_info.return_value = self._mock_service_info_return_value()
         service_uid = "149198"
         service_data = _retrieve_service_data(service_uid)
         expected_keys = [
@@ -34,7 +54,7 @@ class TestDosInterfaceReporting(TestCase):
                 msg = "key " + key + " as lower with or without underscores is NOT present"
                 assert lower in service_data[key] or no_underscore in service_data[key], msg
 
-        assert service_data["CLIENT_NAME"] in ["EditUser", "TestUser"]
+        assert service_data["CLIENT_NAME"] in ["dummy-client"]
         assert "capacity" in service_data["CAPACITY_STATUS"]
 
     def test_get_request_execution_time(self):
