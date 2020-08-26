b66cf53a590e8402f596e2f9e9519e6a8c41a503
Jack Plowman <jack.plowman1@nhs.net>
2020-08-26 08:12:44 +0100

    remove unnecessary make target echos

--- build/automation/lib/slack.mk
+++ build/automation/lib/slack.mk
@@ -1,6 +1,5 @@
 slack-notification: # For local sending message to slack
 	CONTENT=$$(make slack-read-template)
-	echo $$CONTENT
 	curl -X POST -H 'Content-type: application/json' --data "$$CONTENT" $(WEBHOOK_URL)
 
 slack-send-notification: # TODO: Change data to template with overlay of message BUILD NUMBER: $(BUILD_ID): STATUS FAIL etc- MANDATORY PROFILE=[name], GIT_TAG=this.GIT_TAG
@@ -10,7 +9,6 @@ slack-send-notification: # TODO: Change data to template with overlay of message
 	export AWS_SESSION_TOKEN=$${aws[2]}
 	webhook_url=$$(make aws-secret-get NAME=$(PROJECT_GROUP_SHORT)-$(PROJECT_NAME_SHORT)/deployment | jq --raw-output '.webhook_url')
 	CONTENT=$$(make slack-read-template NEW_GIT_TAG=$(GIT_TAG))
-	echo $$CONTENT
 	curl -X POST -H 'Content-type: application/json' --data "$$CONTENT" $$webhook_url
 
 slack-read-template: # Reads text from file and replaces variables in template
