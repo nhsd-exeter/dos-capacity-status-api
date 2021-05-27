5f2498b965ac4739907523591979dbba1d656d9b
Tim Rickwood <tim.rickwood@nhs.net>
2021-05-27 12:30:10 +0100

    Restore use of ALBS plus parameterised SSL cert

--- build/automation/var/platform-texas/v1/account-live-k8s-nonprod.mk
+++ build/automation/var/platform-texas/v1/account-live-k8s-nonprod.mk
@@ -1,6 +1,6 @@
 AWS_ACCOUNT_ID := $(AWS_ACCOUNT_ID_NONPROD)
 AWS_ACCOUNT_NAME := nonprod
-
+AWS_CERTIFICATE = c0718115-4e22-4f48-a4aa-8c16ea86c5e6
 TF_VAR_terraform_platform_state_store = nhsd-texasplatform-terraform-state-store-live-lk8s-$(AWS_ACCOUNT_NAME)
 
 # ==============================================================================
