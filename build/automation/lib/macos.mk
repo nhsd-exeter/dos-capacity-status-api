707e0650068d9e97ee61a2ecaf3e87bb6775a9ac
Jonathan Pearce <j.pearce1@nhs.net>
2021-06-04 09:07:48 +0100

    Update automation scripts (#115)
    * Updates to dev ops scripts
    
    * Refactor
    
    * Update the DevOps automation toolchain scripts to 20210603082020-583e4c3
    
    * Rename make target
    
    * Exclude Python documentation from code formatting
    
    * Keep the editor config unchanged
    
    Co-authored-by: Dan Stefaniuk <daniel.stefaniuk@gmail.com>

--- build/automation/lib/macos.mk
+++ build/automation/lib/macos.mk
@@ -95,7 +95,7 @@ macos-install-essential:: ### Install essential development dependencies - optio
 	# maven depends on java
 	brew $$install maven ||:
 	# Serverless
-	curl -o- -L https://slss.io/install | bash
+	curl -o- -L https://slss.io/install | bash ||:
 
 macos-install-additional:: ### Install additional development dependencies - optional: REINSTALL=true
 	export HOMEBREW_NO_AUTO_UPDATE=1
@@ -351,8 +351,10 @@ _macos-config-oh-my-zsh-make-devops:
 		echo "export KUBECONFIG=~/.kube/configs/lk8s-nonprod-kubeconfig 2> /dev/null"
 		echo
 		echo "# env: Python"
-		echo "export PATH=\$$HOME/.pyenv/bin:\$$PATH"
+		echo "export PYENV_ROOT=$$HOME/.pyenv"
+		echo "export PATH=\$$PYENV_ROOT/bin:\$$PATH"
 		echo "export MYPY_CACHE_DIR=\$$HOME/.mypy_cache"
+		echo "eval \"\$$(pyenv init --path)\""
 		echo "eval \"\$$(pyenv init -)\""
 		echo "eval \"\$$(pyenv virtualenv-init -)\""
 		echo "# env: Go"
